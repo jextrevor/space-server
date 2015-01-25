bd3446bb5dd32c5971880d68fabaf341541229db
Trevor Jex <jextrevor@gmail.com>
2015-01-24 17:13:57 -0700

    Added more music, added explosion simulations

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,9 @@ var stationsocket;
 var conn_options = {
   'sync disconnect on unload':true
 };
+var shake;
+var magnitude = 0;
+var willstop = true;
 var socket = io.connect('http://'+window.location.hostname+':'+window.location.port+'/lobby',conn_options);
 socket.on('lobbyconnect', function(json) {
         document.getElementById("connecting").style.display ="none";
@@ -79,6 +82,13 @@ document.getElementById("errordisconnect").style.display="initial";
 stationsocket.on('connect', function(){
 document.getElementById("errordisconnect").style.display="none";
 });
+stationsocket.on('explosion', function(json){
+    willstop = json['stable'];
+    if(willstop){magnitude += json['magnitude'];}
+    else{magnitude = json['magnitude'];}
+    clearInterval(shake);
+    shake = setInterval(function(){doshake()},1);
+})
 }
 function selectmission(){
     var e = document.getElementById("missionselect");
@@ -89,4 +99,27 @@ function selectstation(){
     var e = document.getElementById("stationselect");
     var strUser = e.options[e.selectedIndex].value;
     socket.emit('lobbyjoin',strUser);
+}
+function doshake(){
+    document.getElementById("briefingcontent").style["-webkit-filter"] = "blur("+magnitude/10+"px)";
+    document.getElementById("briefingcontent").style["filter"] = "blur("+magnitude/10+"px)";
+    document.getElementById("stationpage").style.top = randomIntFromInterval(magnitude * -1, magnitude)+'px';
+    document.getElementById("stationpage").style.left = randomIntFromInterval(magnitude * -1, magnitude)+'px';
+    document.getElementById("briefingcontent").style['-webkit-transform'] = "rotate("+randomIntFromInterval(magnitude * -1, magnitude)/50+'deg)';
+    document.getElementById("briefingcontent").style['-moz-transform'] = "rotate("+randomIntFromInterval(magnitude * -1, magnitude)/50+'deg)';
+    if(willstop){magnitude -= (magnitude+1)/400;}
+    if(magnitude <= 0){
+    magnitude = 0;
+    clearInterval(shake);
+    document.getElementById("stationpage").style.top = '0px';
+    document.getElementById("stationpage").style.left = '0px';
+    document.getElementById("briefingcontent").style["-webkit-filter"] = "blur(0px)";
+    document.getElementById("briefingcontent").style["filter"] = "blur(0px)";
+    document.getElementById("briefingcontent").style['-webkit-transform'] = "rotate(0deg)";
+    document.getElementById("briefingcontent").style['-moz-transform'] = "rotate(0deg)";
+    }
+}
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*((max-min)+1)+min);
 }
